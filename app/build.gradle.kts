3d6b56f704d961cc2ba63e80670bf53684a3bd11
CFChristian <christianfernando671@gmail.com>
2024-11-23 21:27:24 +0700

    Save created note to firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database.ktx)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/project/notesfree/Note_Activity.kt
+++ app/src/main/java/com/project/notesfree/Note_Activity.kt
@@ -1,14 +1,72 @@
 package com.project.notesfree
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.project.notesfree.databinding.ActivityNoteBinding
+import java.util.Date
 
 class Note_Activity : AppCompatActivity() {
+
+    //Deklarasi variabel
+    private lateinit var binding: ActivityNoteBinding
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note)
+        binding = ActivityNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+
+        //Autentikasi user
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            binding.createSaveNote.setOnClickListener {
+                saveNote()
+            }
+        } else {
+            Toast.makeText(this, "User not authenticated", Toast.LENGTH_SHORT).show()
+            finish()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    private fun saveNote() {
+        val title = binding.edtTitleCreateNote.text.toString()
+        val description = binding.edtDescriptionCreateNote.text.toString()
+
+        if (title.isEmpty() && description.isEmpty()) {
+            binding.edtTitleCreateNote.error = "Title is required"
+            binding.edtTitleCreateNote.requestFocus()
+            return
+        }
+
+        val note = Note().apply {
+            this.title = title
+            this.content = description
+            val dateFormat = java.text.SimpleDateFormat("dd/MM/yyyy")
+            this.date = dateFormat.format(Date())
+        }
+
+        val currentUser = auth.currentUser!!
+
+        firestore.collection("users")
+            .document(currentUser.uid)
+            .collection("notes")
+            .add(note)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Failed to save note: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
